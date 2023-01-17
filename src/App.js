72827d5455ef8fa2135ea99c01917d0e8bd500cd
Hisham Abulnaga <h.abulnaga@gmail.com>
2023-01-18 00:04:20 +0200

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 
 import './App.css';
+import {  BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesListPage from './pages/ArticlesListPage';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+        <div className="App">
      <h1>My Awsome Blog</h1>
      <div ID="page-body">
-      Welcome to my blog!
+       <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/articles" element={<ArticlesListPage />} />
+          <Route path="/article/:articleID" element={<ArticlePage />} />
+        </Routes>
       </div>
-
     </div>
+    </BrowserRouter>
+
+
   );
 }
 
